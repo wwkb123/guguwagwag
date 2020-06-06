f857d888276d0bf78ea148e21770b98f140729ee
LandingHeart <land.liu0123@gmail.com>
2020-06-06 14:06:28 -0400

    chat.js have error so i commented out

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Chat from './components/draw/Chat'
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Chat from "./components/draw/Chat";
+import Canvas from "./components/draw/Canvas";
+import "./css/canvas.css";
 function App() {
   return (
     <div>
-      <Chat></Chat>
+      {/* <Chat></Chat> */}
+      <h3 style={{ textAlign: "center" }}>Paint</h3>
+      <div className="main">
+        <div className="color-guide">
+          <h5>Color Guide</h5>
+          <div className="user user">User</div>
+          <div className="user guest">Guest</div>
+        </div>
+
+        <Canvas />
+      </div>
     </div>
   );
 }
